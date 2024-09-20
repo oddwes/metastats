114ae7da0d5ac30cb3e0aaf68fc65520fee9f065
oddwes <oddwes3.0@gmail.com>
2024-09-20 19:35:24 -0400

    removes underline in sidepanel

--- src/components/Sidepanel.jsx
+++ src/components/Sidepanel.jsx
@@ -10,7 +10,7 @@ const Sidepanel = ({setBody}) => {
         <div>
           <Link
             href='#'
-            underline='never'
+            underline='none'
             onClick={() => setBody(<Home />)}
           >
             Home
@@ -19,7 +19,7 @@ const Sidepanel = ({setBody}) => {
         <div>
           <Link
             href='#'
-            underline='never'
+            underline='none'
             onClick={() => setBody(<Transactions />)}
           >
             Transactions
@@ -28,7 +28,7 @@ const Sidepanel = ({setBody}) => {
         <div>
           <Link
             href='#'
-            underline='never'
+            underline='none'
             onClick={() => setBody(<Performance />)}
           >
             Performance
